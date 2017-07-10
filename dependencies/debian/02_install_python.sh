481e9377cd32655750c7a2c1b9c7fb63080f1514
akhanf <akhanf@users.noreply.github.com>
2017-07-10 15:58:09 -0400

    Update 02_install_python.sh
    removed pip installs

--- dependencies/debian/02_install_python.sh
+++ dependencies/debian/02_install_python.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
- apt-get update
- apt-get install -y python3 python3-pip && \
-    pip3 install --upgrade pip && \
-    pip3 install ipython nibabel dipy nipype  matplotlib
+#nipype was causing errors, leaving this out for now:
+
+ #apt-get update
+ #apt-get install -y python3 python3-pip && \
+ #   pip3 install --upgrade pip && \
+ #   pip3 install ipython nibabel dipy nipype  matplotlib
 
 
 
